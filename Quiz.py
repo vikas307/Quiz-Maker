0b4769866489a808af14beaf2a8039342ffbadc0
vikas307 <vikasshenoy5632@gmail.com>
2019-01-11 11:09:12 +0530

    second commit

--- Quiz.py
+++ Quiz.py
@@ -1,9 +1,12 @@
 import numpy as np
 import pandas as pd
 import random
+filename = 'input.csv'
+data = pd.DataFrame(data = pd.read_csv(filename))
 options = np.array(data.iloc[:,1:-1])
 ans = np.array(data.iloc[:,-1])
 ques = np.array(data.iloc[:,0])
+no=random.sample(range(5),2)
 val=0
 no=random.sample(range(5),2)
 for i in no:
